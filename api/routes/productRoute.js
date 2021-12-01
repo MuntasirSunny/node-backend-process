3d3d5d349f0ef03230d2b4a3381707dbe5d0776b
MuntasirSunny <hossainmdmuntasir@gmail.com>
2021-12-01 12:14:06 +0600

    check-auth-on-fileupload

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const upload = require('../middleware/file-upload');
 const productController = require('../controllers/productController');
-const createProduct = require('../controllers/productController');
 
 // Routes
 //router.get('/', checkAuth, userController.getUsers);
 router.get('/', productController.productList);
-router.post('/', upload.single('file'), productController.createProduct);
+router.post('/', checkAuth, upload.single('file'), productController.createProduct);
 
 module.exports = router;
